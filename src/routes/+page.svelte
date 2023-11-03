79ab4c70e6ff11e52b6421d9d8466dd58cdc08ce
Salvador Mercado <msalvadormg@gmail.com>
2023-11-03 02:27:44 -0600

    Tailwind CSS and DaisyUI setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!--
+Ejemplo de como funciona daisy UI
+Agregan su componente (En este caso button)
+Y le agregan las clases de DaisyUI (Checar documentacion)
+Automaticamente le agrega css, similar a bootstrap
+-->
+<button class="btn btn-primary">Button</button>
