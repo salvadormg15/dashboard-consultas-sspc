9aa18eb8c0003b0136c0f07138097214d5d8f711
173037JAGU <173037@upslp.edu.mx>
2023-11-13 00:02:53 -0600

    Request pages changes

--- src/routes/CarRequest/+page.svelte
+++ src/routes/CarRequest/+page.svelte
@@ -1,1 +1,66 @@
-<h1 class="text-center text-2xl">Página de solicitudes</h1>
+<script>
+    function cambioColor(){
+        this.style.background = "#000000";
+        this.style.color = "white";
+    }
+    function regresoColor(){
+        this.style.background = "#F2F2F2";
+        this.style.color = "black";
+    }
+</script>
+
+
+<ul>
+    <li style="padding: 20px;">
+        <div class="collapse bg-base-200" >
+            <input type="checkbox" /> 
+            <div class="collapse-title text-xl font-medium">
+                ID Peticion: 12345
+            </div>
+            <div class="collapse-content"> 
+                <p>Zona: Centro</p>
+                <p>Estado: Pendiente</p>
+                <a href="UserManagement"><button style="border-radius: 5px; border: 1px solid black;">  Entrar  </button></a>
+            </div>
+        </div>
+    </li>
+    <li style="padding: 20px;">
+        <div class="collapse bg-base-200">
+            <input type="checkbox" /> 
+            <div class="collapse-title text-xl font-medium">
+                ID Peticion: 12344
+            </div>
+            <div class="collapse-content"> 
+                <p>Zona: Sur</p>
+                <p>Estado: Pendiente</p>
+                <a href="UserManagement"><button style="border-radius: 5px; border: 1px solid black;">  Entrar  </button></a>
+            </div>
+        </div>
+    </li>
+    <li style="padding: 20px;">
+        <div class="collapse bg-base-200">
+            <input type="checkbox" /> 
+            <div class="collapse-title text-xl font-medium">
+                ID Peticion: 12343
+            </div>
+            <div class="collapse-content"> 
+                <p>Zona: Centro</p>
+                <p>Estado: Pendiente</p>
+                <a href="UserManagement"><button style="border-radius: 5px; border: 1px solid black;">  Entrar  </button></a>
+            </div>
+        </div>
+    </li>
+    <li style="padding: 20px;">
+        <div class="collapse bg-base-200">
+            <input type="checkbox" /> 
+            <div class="collapse-title text-xl font-medium">
+                ID Peticion: 12342
+            </div>
+            <div class="collapse-content"> 
+                <p>Zona: Este</p>
+                <p>Estado: Completada</p>
+                <a href="UserManagement"><button style="border-radius: 5px; border: 1px solid black;">  Entrar  </button></a>
+            </div>
+        </div>
+    </li>
+</ul>
--- src/routes/UserManagement/+page.svelte
+++ src/routes/UserManagement/+page.svelte
@@ -1,1 +1,78 @@
-<h1 class="text-center text-2xl">Página de administración de usuarios</h1>
+<script>
+    function cambioColor(){
+        this.style.background = "#000000";
+        this.style.color = "white";
+    }
+    function regresoColor(){
+        this.style.background = "#F2F2F2";
+        this.style.color = "black";
+    }
+</script>
+
+<ul>
+    <li style="padding: 20px;">
+        <div class="form-control w-full max-w-xs">
+            <div class="collapse-title text-xl font-medium">
+                Dato 1
+            </div>
+            <input
+                type="text"
+                placeholder="Llenar este campo"
+                class="input input-bordered w-full max-w-xs"
+            />
+        </div>
+    </li>
+
+    <li style="padding: 20px;">
+        <div class="form-control w-full max-w-xs">
+            <div class="collapse-title text-xl font-medium">
+                Dato 2
+            </div>
+            <input
+                type="text"
+                placeholder="Llenar este campo"
+                class="input input-bordered w-full max-w-xs"
+            />
+        </div>
+    </li>
+    <li style="padding: 20px;">
+        <div class="form-control w-full max-w-xs">
+            <div class="collapse-title text-xl font-medium">
+                Dato 3
+            </div>
+            <input
+                type="text"
+                placeholder="Llenar este campo"
+                class="input input-bordered w-full max-w-xs"
+            />
+        </div>
+    </li>
+    <li style="padding: 20px;">
+        <div class="form-control w-full max-w-xs">
+            <div class="collapse-title text-xl font-medium">
+                Dato 4
+            </div>
+            <input
+                type="text"
+                placeholder="Llenar este campo"
+                class="input input-bordered w-full max-w-xs"
+            />
+        </div>
+    </li>
+    <li style="padding: 20px;">
+        <div class="form-control w-full max-w-xs">
+            <div class="collapse-title text-xl font-medium">
+                Dato 5
+            </div>
+            <input
+                type="text"
+                placeholder="Llenar este campo"
+                class="input input-bordered w-full max-w-xs"
+            />
+        </div>
+    </li>
+    <li style="padding: 20px;">
+        <a href="."><button class="btn btn-success">Consultar sistema externo</button></a>
+        <a href="CarRequest"><button class="btn btn-success">Enviar Información</button></a>
+    </li>
+</ul>
