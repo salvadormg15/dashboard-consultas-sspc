79e34f89cbaa4e09587c24a36b3eb7511e853f8a
Glosaani <176062@upslp.edu.mx>
2023-12-01 00:07:00 -0600

    Update management

--- src/routes/UserManagement/+page.svelte
+++ src/routes/UserManagement/+page.svelte
@@ -9,28 +9,42 @@
     let showSuccessMessage = false;
     let showSuccessMessage2 = false;
     let volver = false;
+    let showModifyModal = false;
+    
 	let users = [
-		{ name: 'John Doe', role: 'Developer' },
-		{ name: 'Jane Smith', role: 'Designer' },
-		{ name: 'Bob Johnson', role: 'Manager' }
-		// Puedes añadir más usuarios a la lista
-	];
+    { name: 'John ',name2:'Doe', role: 'Developer' },
+    { name: 'Jane ',name2: 'Smith' , role: 'Designer' },
+    { name: 'Bob ',name2: 'Johnson', role: 'Manager' }
+  ];
+    let showModal = null;
+    let showModal2 = null;
+    let showModal3 = null;
+    let selectedUser = null;
 
-    function handleDelete() {
-     showSuccessMessage = true;
-    }
+    function openEditModal(user) {
+    selectedUser = user;
+    showModal = 'edit';
+  }
 
-    function handleCancel() {
-     showSuccessMessage = true;
-    }
+  function openDeleteModal(user) {
+    selectedUser = user;
+    showModal3 = 'delete';
+  }
 
+  function closeModals() {
+    showModal = null;
+    selectedUser = null;
+  }
+    
     function reloadPage() {
      location.reload();
     }
 
-    function modificar(){
-        window.location.href = "/UserManagement/EditUser";
+    function modificarYes(){
+        showModal2 = 'listo';
     }
+
+
     
 </script>
 
@@ -81,43 +95,109 @@
 </div>
 
 <div class="p-4 mx-auto max-w-3xl">
-	<h1 class="text-center text-2xl mb-4">Tabla de Usuarios</h1>
-	<div class="overflow-x-auto">
-		<table class="table w-full border border-solid border-cyan-400">
-			<thead>
-				<tr>
-					<th class="text-left py-2">Nombre</th>
-					<th class="text-left py-2">Apellidos</th>
-					<th class="text-left py-2">Cargo</th>
-					<th class="text-left py-2">Acciones</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each users as user}
-					<tr>
-						<td class="py-2">{user.name}</td>
-						<td class="py-2">{user.name}</td>
-						<td class="py-2">{user.role}</td>
-						<td class="py-2">
-							<div class="flex space-x-2">
-								<button class="btn btn-warning"  on:click={modificar} >
-									<PencilBox />
-								</button>
-                                
-								<button class="btn btn-error" >
-									<DeleteCircle />
-								</button>
-                               
-							</div>
-						</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-	</div>
+    <h1 class="text-center text-2xl mb-4">Tabla de Usuarios</h1>
+    <div class="overflow-x-auto">
+      <table class="table w-full border border-solid border-cyan-400">
+        <thead>
+          <tr>
+            <th class="text-left py-2">Nombre</th>
+            <th class="text-left py-2">Apellidos</th>
+            <th class="text-left py-2">Cargo</th>
+            <th class="text-left py-2">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each users as user}
+            <tr>
+              <td>{user.name}</td>
+              <td>{user.name2}</td>
+              <td>{user.role}</td>
+              <td>
+                <div class="flex space-x-2">
+                    <button on:click={() => {
+                        console.log('Edit button clicked');
+                        openEditModal(user);
+                      }} class="btn btn-warning">
+                        <PencilBox />
+                      </button>
+                      <button on:click={() => {
+                        console.log('Delete button clicked');
+                        openDeleteModal(user);
+                      }} class="btn btn-error">
+                        <DeleteCircle />
+                      </button>
+                </div>
+              </td>
+            </tr>
+          {/each}
+      
+        </tbody>
+      </table>
+    </div>
+    {#if showModal === 'edit' }
+        <div class="dark-overlay"></div>
+        <div class="card w-96 bg-base-100 shadow-xl fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  p-4 rounded-md " >
+        <div class="card-body">
+            
+            <div class="container">
+            <div class="avatar">
+                <div class="container2">
+                    <AccountCircle width="70px" height="70px" />
+                </div>
+            </div>
+            <p>Nombre</p>
+            <input type="text" placeholder="{selectedUser ? selectedUser.name : 'N/A'}" class="input input-bordered input-primary w-full max-w-xs" />
+            <p>Apellido</p>
+            <input type="text" placeholder="{selectedUser ? selectedUser.name2 : 'N/A'}" class="input input-bordered input-primary w-full max-w-xs" />
+            <p>Ocupación</p>
+            <input type="text" placeholder="{selectedUser ? selectedUser.role : 'N/A'}" class="input input-bordered input-primary w-full max-w-xs" />
+            <br><br>
+            <button class="btn btn-active btn-neutral" on:click={reloadPage}>Cancelar</button>
+            <button class="btn btn-active btn-primary" on:click={reloadPage}>Modificar</button>
+            
+        
+        </div>
+        
+        </div>
+        </div>
+{/if}
+{#if showModal3 === 'delete' }
+        <div class="dark-overlay"></div>
+        <div class="card w-96 bg-primary text-primary-content fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  p-4 rounded-md " >
+        <div class="card-body">
+    
+            <div class="container">
+            <h2 class="card-title" style="color: black;">¿Desea eliminar a este usuario?</h2>
+            
+            <br><br>
+            <button class="btn btn-active btn-neutral" on:click={reloadPage}>No</button>
+            <button class="btn btn-active btn-primary" on:click={reloadPage}>Sí</button>
+
+            </div>
+        
+        </div>
+        </div>
+
+
+{/if}
+<!--
+        {#if showModal2 === 'listo' }
+        <div class="dark-overlay"></div>
+        <div class="card w-96 bg-primary text-primary-content fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  p-4 rounded-md " >
+        <div class="card-body">
+            
+            <div class="container">
+                <h2 class="card-title">Usuario modificado</h2>
+                <div class="card-actions justify-end">
+                  <button class="btn" on:click={reloadPage}>Salir</button>
+                </div>
+            </div>
+            </div>
+            </div>
+        {/if}-->
 </div>
-
-
+  
+  
 <style>
    
     .container{
@@ -126,5 +206,24 @@
     .container2{
         text-align: center;
     }
-
+    
+   .card{
+    text-align: center;
+    z-index: 1000;
+    background-color:white;
+        transition: all .5s;
+    }
+    .dark-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.3); 
+        z-index: 999;
+}
+    input::placeholder {
+    color: black; 
+    }
+    
 </style>
